82349e1b5599cb8fc30f43bc001a8c563ac5646d
Samuel Meenzen <samuel@meenzen.net>
2023-11-23 10:34:50 +0100

    fix: delete Cross.toml after compiling

--- scripts/build-rust-sdk.sh
+++ scripts/build-rust-sdk.sh
@@ -4,9 +4,11 @@ set -eo pipefail
 
 echo "=> Building rust-sdk..."
 
+BASE_DIRECTORY=$(pwd)
 SDK_DIRECTORY=external/matrix-rust-sdk/bindings/matrix-sdk-ffi
+CROSS_TOML_FILE=external/matrix-rust-sdk/Cross.toml
 
-cp Cross.toml external/matrix-rust-sdk/Cross.toml
+cp Cross.toml $CROSS_TOML_FILE
 cd $SDK_DIRECTORY
 
 echo "=> Starting build for target: x86_64-pc-windows-gnu"
@@ -20,3 +22,6 @@ cross build --target x86_64-unknown-linux-gnu --release
 
 echo "=> Starting build for target: aarch64-unknown-linux-gnu"
 cross build --target aarch64-unknown-linux-gnu --release
+
+cd $BASE_DIRECTORY
+rm $CROSS_TOML_FILE
